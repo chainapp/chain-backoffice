355cbe9e0b8f15fd52e44d9bf1b73de4c2e73bb7
Louis Stockreisser <louis.stockreisser@worldline.com>
2015-09-23 13:29:47 +0200

    [LST] fix chains table

--- dist/js/angular/controllers/chainsCtrl.js
+++ dist/js/angular/controllers/chainsCtrl.js
@@ -14,15 +14,15 @@ $scope.init = function(){
         console.log('chains total is '+res.length)
         $scope.tableParams = new ngTableParams({
 	        page: 1,            // show first page
-	        count: 10,
+	        count: 100,
 	        sorting: {
             	created_at: 'desc'     // initial sorting
         	}          // count per page
 	    }, {
-	        total: res.length, // length of data
+	        total: $scope.chains.length, // length of data
 	        getData: function($defer, params) {
 	        	var orderedData = params.sorting ?
-                        $filter('orderBy')(res, params.orderBy()) : res;
+                        $filter('orderBy')($scope.chains, params.orderBy()) : $scope.chains;
                 orderedData = params.filter ?
                         $filter('filter')(orderedData, params.filter()) : orderedData;
 	            $scope.chains = orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count());
@@ -39,7 +39,7 @@ $scope.init();
         chainService.delete(chain._id).then(function(res){
             $scope.alerts.push({title:'#'+chain.title});
             $timeout(function(){ $scope.alerts.splice(0,1);console.log("timeout"); }, 3000);
-            init();
+            $scope.init();
         })
     }
 
